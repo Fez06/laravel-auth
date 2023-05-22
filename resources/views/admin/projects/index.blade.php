1573165847088266b6d300d81b51820f09e3f2fe
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-05-22 16:17:19 +0200

    aggiunto un po' di stile

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -5,17 +5,23 @@
 
 @foreach ($projects as $project)
 
-<div>
-    <h2><a href="{{ route('admin.projects.show', $project) }}">{{$project->title}}</a></h2>
-    <a href="{{ route('admin.projects.edit', $project) }}"><button type="button">Modifica</button></a>
-    <form action="{{ route('admin.projects.destroy', $project->id) }}"
-        method="POST">
-        @csrf
-        @method('DELETE')
-        
-        <input type="submit" id="delete" value="Elimina">
-    </form>
-</div>
+<ul class="list-unstyled d-flex m-0 gap-1 justify-content">
+    <li>
+        <h2><a href="{{ route('admin.projects.show', $project) }}" class="btn btn-sm btn-primary">{{$project->title}}</a></h2>
+    </li>
+    <li>
+        <a href="{{ route('admin.projects.edit', $project) }}"><button type="button" class="btn btn-sm btn-warning">Modifica</button></a>
+    </li>
+    <li>
+        <form action="{{ route('admin.projects.destroy', $project->id) }}"
+            method="POST">
+            @csrf
+            @method('DELETE')
+            
+            <input type="submit" id="delete" value="Elimina" class="btn btn-sm btn-danger">
+        </form>
+    </li>
+</ul>
  
 @endforeach
 
